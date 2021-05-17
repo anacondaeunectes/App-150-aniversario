8eea0f9afb2b307f79ea1792e3d06469ba29f39e
Pedro <pedro.arevalo.alcaide@gmail.com>
2021-05-17 16:43:55 +0200

    solucionado problema del service

---
+++ src/app/saludos/saludos.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-saludos',
+  templateUrl: './saludos.page.html',
+  styleUrls: ['./saludos.page.scss'],
+})
+export class SaludosPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
